a25a0118a5a0ab7f9a9f926ae817a97ba8e4ca41
Matheus Leite <matheusleitecordeiro@gmail.com>
2026-09-16 08:58:54 -0300

    adicionando coluna name na tabela users

--- src/config/database.js
+++ src/config/database.js
@@ -24,6 +24,7 @@ async function createTables() {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
+        name TEXT NOT NULL,
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
       );
